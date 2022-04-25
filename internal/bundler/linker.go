1eb35920aab013a783acaca5f87e1fd8abe67266
hardfist <yangjianzju@gmail.com>
2022-04-25 14:03:52 +0800

    chore: fix css tree shaking

--- internal/bundler/linker.go
+++ internal/bundler/linker.go
@@ -3024,10 +3024,11 @@ func (c *linkerContext) findImportedCSSFilesInJSOrder(entryPoint uint32) (order 
 		// Iterate over the associated CSS imports in postorder
 		if repr.CSSSourceIndex.IsValid() {
 			cssIndex := repr.CSSSourceIndex.GetIndex()
-			cssFile := c.graph.Files[cssIndex]
-			if parentIsLive && cssFile.IsLive {
-				order = append(order, cssIndex)
-			}
+			// cssFile := c.graph.Files[cssIndex]
+			order = append(order, cssIndex)
+			// if parentIsLive && cssFile.IsLive {
+			// 	order = append(order, cssIndex)
+			// }
 
 		}
 	}
